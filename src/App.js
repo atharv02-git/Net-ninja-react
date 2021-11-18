f00c371ad42862108ca7d7d3de110173b462e411
Atharv <atharvbhandare2001@gmail.com>
2021-11-18 13:23:15 +0530

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+const[name,setName] = useState('Hodor')
+
+const changeNameHandler = () => {
+  setName('Arya')
+  console.log(name)
+}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={changeNameHandler}>Change Name</button>
     </div>
   );
-}
+};
 
 export default App;
