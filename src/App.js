6ae70e58142f7351ab7b830b8d36c6b0869b34e2
Atharv <atharvbhandare2001@gmail.com>
2021-11-19 13:31:58 +0530

    show & Hide Events

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
+  const[showEvents,setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
     { title: "race on moo moo farm", id: 3 },
   ]);
+
+  console.log(showEvents)
 
   const deleteHandler = (id) => {
     setEvents((prevEvents) => {
@@ -23,7 +26,19 @@ const App = () => {
 
   return (
     <div className="App">
-      {events.map((event, index) => (
+      {showEvents && (
+      <div>
+        <button onClick={() => setShowEvents(false)}>Hide Events</button>
+      </div>
+      )}
+
+      {!showEvents && (
+      <div>
+        <button onClick={() => setShowEvents(true)}>Show Events</button>
+      </div>
+      )}
+
+      {showEvents && events.map((event, index) => (
         <div key={event.id}>
           <h2>
             {index} - {event.title}
