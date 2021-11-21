7263260132485d192f1b899d29a80971fd9b9492
Atharv <atharvbhandare2001@gmail.com>
2021-11-21 14:26:50 +0530

    working with css modules

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./EventList.module.css";
 
 export default function EventList({eventsArr,deleteHandler}) {
   return (
     <div>
       {eventsArr.map((event, index) => (
-        <div key={event.id}>
+        <div className={styles.card} key={event.id}>
         <h2>{index} - {event.title}</h2>
         <button onClick={() => {deleteHandler(event.id)}}> Delete event </button>
       </div>
