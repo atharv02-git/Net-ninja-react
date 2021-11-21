18cd71e48e00297ccf24624bca9a219e40a43485
Atharv <atharvbhandare2001@gmail.com>
2021-11-21 20:39:36 +0530

    working with onChange event property

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,17 +1,21 @@
 import "./EventForm.css";
+import { useState} from 'react';
 
 export default function EventForm() {
+    const[title,setTitle] = useState('')
+    const[date,setDate] = useState('')
   return (
     <div>
       <form className="new-event-form">
           <label>
               <span>Enter event : </span>
-              <input type="text" />
+              <input type="text" onChange={(e) => setTitle(e.target.value)}/>
           </label>
           <label>
               <span>Date : </span>
-              <input type="date" />
+              <input type="date" onChange={(e) => setDate(e.target.value)}/>
           </label>
+          <p>Title: {title}, Date: {date}</p>
           <button className="submit-button">Submit</button>
       </form>
     </div>
