64a4e263ebf5ed5fb2d1b585105ccd62fc033550
Atharv <atharvbhandare2001@gmail.com>
2021-11-21 21:25:15 +0530

    handling submit events

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,22 +1,48 @@
 import "./EventForm.css";
-import { useState} from 'react';
+import { useState } from "react";
 
 export default function EventForm() {
-    const[title,setTitle] = useState('')
-    const[date,setDate] = useState('')
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  //   Reset function
+  const resetHandler = () => {
+    setTitle("");
+    setDate("");
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const event = {
+        title: title,
+        date: date,
+        id: Math.floor(Math.random() * 10000)
+    }
+    console.log(event)
+    resetHandler()
+  };
+
   return (
     <div>
-      <form className="new-event-form">
-          <label>
-              <span>Enter event : </span>
-              <input type="text" onChange={(e) => setTitle(e.target.value)}/>
-          </label>
-          <label>
-              <span>Date : </span>
-              <input type="date" onChange={(e) => setDate(e.target.value)}/>
-          </label>
-          <p>Title: {title}, Date: {date}</p>
-          <button className="submit-button">Submit</button>
+      <form className="new-event-form" onSubmit={submitHandler}>
+        <label>
+          <span>Enter event : </span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Date : </span>
+          <input
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+          />
+        </label>
+        <button className="submit-button">Submit</button>
       </form>
     </div>
   );
